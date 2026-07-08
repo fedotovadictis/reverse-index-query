86e6d6cb29614e7709401ec386799dd1d280f07e
fedotovadictis <dashatagangrog2004@gmail.com>
2026-07-08 11:13:02 +0300

    Add index building and ID sorting

--- cmd/reverse-index-query/main.go
+++ cmd/reverse-index-query/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"project_cat_reverse/internal/index"
-	"project_cat_reverse/internal/reader"
 )
 
 func main() {
@@ -14,18 +13,33 @@ func main() {
 			return
 		}
 	*/
+	/*
+		events, err := reader.ReadEvents("events.jsonl")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
-	events, err := reader.ReadEvents("events.jsonl")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+		idx := index.NewIndex()
 
+		for _, evt := range events {
+			idx.AddDepartment(evt)
+		}
+
+		fmt.Println(idx.Fields)
+	*/
 	idx := index.NewIndex()
 
-	for _, evt := range events {
-		idx.AddDepartment(evt)
-	}
+	idx.AddField("department", "sales", 10)
+	idx.AddField("department", "sales", 3)
+	idx.AddField("department", "sales", 7)
+	idx.AddField("department", "sales", 1)
 
+	fmt.Println("До сортировки:")
+	fmt.Println(idx.Fields)
+
+	idx.Sort()
+
+	fmt.Println("После сортировки:")
 	fmt.Println(idx.Fields)
 }
--- internal/index/index.go
+++ internal/index/index.go
@@ -1,6 +1,9 @@
 package index
 
-import "project_cat_reverse/internal/event"
+import (
+	"project_cat_reverse/internal/event"
+	"sort"
+)
 
 type Index struct {
 	Fields map[string]map[string][]uint64
@@ -11,18 +14,46 @@ func NewIndex() Index {
 		Fields: make(map[string]map[string][]uint64),
 	}
 }
-
-func (idx *Index) AddDepartment(evt event.Event) {
-	departmentMap, ok := idx.Fields["department"]
-	if !ok {
-		departmentMap = make(map[string][]uint64)
+func (idx *Index) AddField(field string, value string, id uint64) {
+	if value == "" {
+		return
 	}
-	idx.Fields["department"] = departmentMap
+	fieldMap, ok := idx.Fields[field]
+	if !ok {
+		fieldMap = make(map[string][]uint64)
+	}
 
-	ids, ok := departmentMap[evt.Department]
+	ids, ok := fieldMap[value]
 	if !ok {
 		ids = make([]uint64, 0)
 	}
-	ids = append(ids, evt.ID)
-	departmentMap[evt.Department] = ids
+	ids = append(ids, id)
+	fieldMap[value] = ids
+	idx.Fields[field] = fieldMap
+}
+func (idx *Index) AddEvent(evt event.Event) {
+	idx.AddField("department", evt.Department, evt.ID)
+	idx.AddField("action", evt.Action, evt.ID)
+	idx.AddField("channel", evt.Channel, evt.ID)
+	idx.AddField("file_ext", evt.FileExt, evt.ID)
+	idx.AddField("destination_type", evt.DestinationType, evt.ID)
+	idx.AddField("severity", evt.Severity, evt.ID)
+	idx.AddField("user_id", evt.UserID, evt.ID)
+}
+
+func (idx *Index) Build(events []event.Event) {
+	for _, evt := range events {
+		idx.AddEvent(evt)
+	}
+
+}
+func (idx *Index) Sort() {
+	for _, values := range idx.Fields {
+		for value, ids := range values {
+			sort.Slice(ids, func(i, j int) bool {
+				return ids[i] < ids[j]
+			})
+			values[value] = ids
+		}
+	}
 }
